4ad78a8d44bf26ef930f67453fbcd5f786b7c76d
Marcelo Rubens <marcelo.rubens@hotmail.com>
2013-10-14 13:03:51 -0300

    Adicionando as hipermídias

---
+++ hipermidias/js/sidebar.js
@@ -0,0 +1,43 @@
+function toggleBar1()
+{
+  document.getElementById('saiba-mais').classList.toggle("open");
+}
+
+var abridoresDaBarra = document.getElementsByClassName("abrebarra1");
+
+for(var i = 0; i < abridoresDaBarra.length; i++) {
+  abridoresDaBarra[i].onclick = toggleBar1;
+}
+
+function toggleBar2()
+{
+  document.getElementById('glossario').classList.toggle("open");
+}
+
+var abridoresDaBarra = document.getElementsByClassName("abrebarra2");
+
+for(var i = 0; i < abridoresDaBarra.length; i++) {
+  abridoresDaBarra[i].onclick = toggleBar2;
+}
+
+function toggleBar3()
+{
+  document.getElementById('saiba-mais2').classList.toggle("open");
+}
+
+var abridoresDaBarra = document.getElementsByClassName("abrebarra3");
+
+for(var i = 0; i < abridoresDaBarra.length; i++) {
+  abridoresDaBarra[i].onclick = toggleBar3;
+}
+
+function toggleBar4()
+{
+  document.getElementById('glossario2').classList.toggle("open");
+}
+
+var abridoresDaBarra = document.getElementsByClassName("abrebarra4");
+
+for(var i = 0; i < abridoresDaBarra.length; i++) {
+  abridoresDaBarra[i].onclick = toggleBar4;
+}
