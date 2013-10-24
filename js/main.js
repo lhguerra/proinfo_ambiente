b28b28f63449b16a2057f6d84336ccd48e1cd00a
lhguerra <lhguerra@gmail.com>
2013-10-24 20:05:06 -0200

    fixments

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// custom scrollbar
+$(document).ready(function() {
+  $(".scroll").customScrollbar({
+    skin: "default-skin", 
+    hScroll: false
+  });
+});
+
+// increase font-size
+$(document).ready(function() {
+ 
+  // aumentando a fonte
+  $(".inc-font").click(function () {
+    var size = $("body").css('font-size');
+         
+    size = size.replace('px', '');
+    size = parseInt(size) + 2;
+         
+    $("body").animate({'font-size' : size + 'px'});
+  });
+       
+  //diminuindo a fonte
+  $(".dec-font").click(function () {
+    var size = $("body").css('font-size');
+         
+    size = size.replace('px', '');
+    size = parseInt(size) - 2;
+         
+    $("body").animate({'font-size' : size + 'px'});
+  });
+       
+  // resetando a fonte
+  $(".res-font").click(function () {   
+    $("body").animate({'font-size' : '14px'});
+  });
+});
+
+// profile menu
+$(function() {
+  var profileMenu = $('#user'),
+      profileBox = $('#user-modal');
+    
+  profileMenu.bind('click', function() {
+    profileBox.toggleClass('appear');
+  });
+});
+
+// hipermidiaMenu
+$(function () {
+  var hipermidiaMenu = $('.hipermidia-show');
+
+  hipermidiaMenu.bind('click', function(foo) {
+    $('.appear').removeClass('appear');
+    foo.preventDefault();
+    foo.stopImmediatePropagation()
+    var target = $(this).attr('data-target');
+    $('#' + target).toggleClass('appear');
+
+    $(".scroll").customScrollbar({
+      skin: "default-skin",
+      hScroll: false
+    });
+  });
+
+  $(window).bind('click', function(ev) {
+      $('.appear').removeClass('appear');
+    });
+});
